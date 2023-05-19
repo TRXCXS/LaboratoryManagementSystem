39ae0e7a01999ccf63d6e025c2fdf32572da3272
Runtian <zrt0511@qq.com>
2023-05-19 13:23:32 +0800

    更新了LongArrangement和Laboratory相关代码，其中Laboratory有较大变化

---
+++ Backend/src/main/java/com/example/backend/controller/requestbody/LabRequestBody1.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller.requestbody;
+
+import com.example.backend.utils.enumClasses.model.LabType;
+import com.example.backend.utils.enumClasses.model.Slot;
+import com.example.backend.utils.enumClasses.model.Weekday;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LabRequestBody1 {
+    private Integer startWeek;
+    private Integer endWeek;
+    private Weekday weekday;
+    private Slot slot;
+    private LabType labType;
+}
