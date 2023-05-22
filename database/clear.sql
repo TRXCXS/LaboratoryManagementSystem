0ca515b6bbd1b106a9f8b60d8d7610ebd7c6e1b3
TRXCX <2944859506@qq.com>
2023-05-22 19:38:27 +0800

    clear.sql可以清空所有表的数据，同时主键归零，用于插入数据之前使用；laboratory_with_init_data.sql是实验室包含初始数据的SQL文件。

---
+++ database/clear.sql
@@ -0,0 +1,17 @@
+truncate table `user`;
+truncate table `user_role`;
+truncate table `administrator`;
+truncate table `instructor`;
+truncate table `technician`;
+truncate table `student`;
+
+truncate table `student_request`;
+truncate table `instructor_request`;
+truncate table `repair_request`;
+truncate table `short_arrangement`;
+truncate table `long_arrangement`;
+
+truncate table `current_semester`;
+truncate table `semester`;
+truncate table `laboratory`;
+truncate table `responsible_for`;
