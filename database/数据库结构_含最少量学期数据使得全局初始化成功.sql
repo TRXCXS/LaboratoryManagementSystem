261584f4c879616659a905d3a1d8e3a6fa49184a
Runtian <zrt0511@qq.com>
2023-05-25 18:27:13 +0800

    增加一个含最少量数据，使得后端可以初始化的数据库脚本

---
+++ database/数据库结构_含最少量学期数据使得全局初始化成功.sql
@@ -0,0 +1,288 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysqlconn1
+ Source Server Type    : MySQL
+ Source Server Version : 80031 (8.0.31)
+ Source Host           : localhost:3306
+ Source Schema         : demo
+
+ Target Server Type    : MySQL
+ Target Server Version : 80031 (8.0.31)
+ File Encoding         : 65001
+
+ Date: 25/05/2023 18:22:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for administrator
+-- ----------------------------
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE `administrator`  (
+  `adminID` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`adminID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of administrator
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for current_semester
+-- ----------------------------
+DROP TABLE IF EXISTS `current_semester`;
+CREATE TABLE `current_semester`  (
+  `queryID` int NOT NULL,
+  `currentSemesterID` int NULL DEFAULT NULL,
+  PRIMARY KEY (`queryID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of current_semester
+-- ----------------------------
+INSERT INTO `current_semester` VALUES (1, 1);
+
+-- ----------------------------
+-- Table structure for instructor
+-- ----------------------------
+DROP TABLE IF EXISTS `instructor`;
+CREATE TABLE `instructor`  (
+  `instructorID` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`instructorID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of instructor
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for instructor_request
+-- ----------------------------
+DROP TABLE IF EXISTS `instructor_request`;
+CREATE TABLE `instructor_request`  (
+  `instructorRequestID` int NOT NULL AUTO_INCREMENT,
+  `requestTime` datetime NULL DEFAULT NULL,
+  `labType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `startWeek` int NULL DEFAULT NULL,
+  `endWeek` int NULL DEFAULT NULL,
+  `weekday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `slot` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `studentClass` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `studentCount` int NULL DEFAULT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `adminProcessTime` datetime NULL DEFAULT NULL,
+  `adminMessage` varchar(1022) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `instructorID` int NOT NULL,
+  `semesterID` int NOT NULL,
+  PRIMARY KEY (`instructorRequestID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of instructor_request
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for laboratory
+-- ----------------------------
+DROP TABLE IF EXISTS `laboratory`;
+CREATE TABLE `laboratory`  (
+  `labID` int NOT NULL AUTO_INCREMENT,
+  `labNumber` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `labType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `deviceCount` int NULL DEFAULT NULL,
+  PRIMARY KEY (`labID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of laboratory
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for long_arrangement
+-- ----------------------------
+DROP TABLE IF EXISTS `long_arrangement`;
+CREATE TABLE `long_arrangement`  (
+  `longArrangementID` int NOT NULL AUTO_INCREMENT,
+  `startWeek` int NULL DEFAULT NULL,
+  `endWeek` int NULL DEFAULT NULL,
+  `weekday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `slot` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `studentClass` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `studentCount` int NULL DEFAULT NULL,
+  `instructorRequestID` int NOT NULL,
+  `labID` int NOT NULL,
+  PRIMARY KEY (`longArrangementID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of long_arrangement
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for repair_request
+-- ----------------------------
+DROP TABLE IF EXISTS `repair_request`;
+CREATE TABLE `repair_request`  (
+  `repairRequestID` int NOT NULL AUTO_INCREMENT,
+  `requestTime` datetime NULL DEFAULT NULL,
+  `requestDescription` varchar(1022) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `repairStartTime` datetime NULL DEFAULT NULL,
+  `repairEndTime` datetime NULL DEFAULT NULL,
+  `technicianMessage` varchar(1022) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `instructorID` int NOT NULL,
+  `labID` int NOT NULL,
+  PRIMARY KEY (`repairRequestID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of repair_request
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for responsible_for
+-- ----------------------------
+DROP TABLE IF EXISTS `responsible_for`;
+CREATE TABLE `responsible_for`  (
+  `responsibleForID` int NOT NULL AUTO_INCREMENT,
+  `technicianID` int NOT NULL,
+  `labID` int NOT NULL,
+  PRIMARY KEY (`responsibleForID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of responsible_for
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for semester
+-- ----------------------------
+DROP TABLE IF EXISTS `semester`;
+CREATE TABLE `semester`  (
+  `semesterID` int NOT NULL AUTO_INCREMENT,
+  `firstHalfYear` int NULL DEFAULT NULL,
+  `secondHalfYear` int NULL DEFAULT NULL,
+  `season` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `startDate` datetime NULL DEFAULT NULL,
+  `weekCount` int NULL DEFAULT NULL,
+  PRIMARY KEY (`semesterID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of semester
+-- ----------------------------
+INSERT INTO `semester` VALUES (1, 2022, 2023, 'FALL', '2023-05-15 18:06:03', 18);
+
+-- ----------------------------
+-- Table structure for short_arrangement
+-- ----------------------------
+DROP TABLE IF EXISTS `short_arrangement`;
+CREATE TABLE `short_arrangement`  (
+  `shortArrangementID` int NOT NULL AUTO_INCREMENT,
+  `week` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `weekday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `slot` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `studentRequestID` int NOT NULL,
+  `labID` int NOT NULL,
+  PRIMARY KEY (`shortArrangementID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of short_arrangement
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `studentID` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `clazz` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`studentID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for student_request
+-- ----------------------------
+DROP TABLE IF EXISTS `student_request`;
+CREATE TABLE `student_request`  (
+  `studentRequestID` int NOT NULL AUTO_INCREMENT,
+  `week` int NULL DEFAULT NULL,
+  `weekday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `slot` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `reason` varchar(1022) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `requestTime` datetime NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `adminProcessTime` datetime NULL DEFAULT NULL,
+  `adminMessage` varchar(1022) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `useCompleteTime` datetime NULL DEFAULT NULL,
+  `labID` int NOT NULL,
+  `semesterID` int NOT NULL,
+  `studentID` int NOT NULL,
+  PRIMARY KEY (`studentRequestID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of student_request
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for technician
+-- ----------------------------
+DROP TABLE IF EXISTS `technician`;
+CREATE TABLE `technician`  (
+  `technicianID` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`technicianID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of technician
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `loginID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`userID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `userRoleID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`userRoleID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
